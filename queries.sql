11fb1a2f9b250146b8a94c5abf14b524bf33392a
Jennifer Morgan <jemorgaine@gmail.com>
2021-02-06 15:10:52 -0600

    - commit with employer and skills add working and connected to SQL - done through part 2

--- queries.sql
+++ queries.sql
@@ -8,6 +8,15 @@ Name = VARCHAR(255)
 Skills = VARCHAR(255)
 
 ## Part 2: Test it with SQL
+SELECT * FROM techjobs.employer
+WHERE location = "Saint Louis, MO"
+OR location = "St. Louis, MO"
+OR location = "Saint Louis, Missouri"
+OR location = "St. Louis, Missouri"
+OR location = "Saint Louis"
+OR location = "St. Louis"
+OR location = "St. Louis City"
+OR location - "St. Louis County"
 
 ## Part 3: Test it with SQL
 
